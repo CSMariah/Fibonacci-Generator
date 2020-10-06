e3d5a2d11d45703dcd346d103c54bb6010e591ca
Mariah KH <maro0osha1994@gmail.com>
2020-10-06 18:59:29 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function fibonacciGenerator(n)
+{
+  var output = [];
+  if (n == 0) {
+    output = ["Nothing"];
+  }
+  else if (n == 1) {
+    output = [0];
+  } else if (n == 2) {
+    output = [0, 1];
+  } else {
+    output = [0, 1];
+    for (var i = 2; i < n; i++) {
+      output.push(output[output.length-2]+output[output.length-1]);
+    }
+  }
+  return output;
+};
+
+function generate() {
+  var num = document.getElementById("fnumber").value;
+  var output = fibonacciGenerator(num);
+  document.getElementById("fnumbers").innerHTML = "The "+ num + " Fibonacci Numbers are: \n"+ output.join('\r\n');
+  alert("The " + num + " Fibonacci Numbers are: \n" + output);
+}
